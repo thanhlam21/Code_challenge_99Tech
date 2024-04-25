63d09e6600e45c33ceb65143069c46f5688f8764
thanhlam21 <thanhlam110321@gmail.com>
2024-04-25 19:00:49 +0700

    code challenge

---
+++ problem2/fancy-form/src/App.js
@@ -0,0 +1,13 @@
+// File: App.js
+import React from "react";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
